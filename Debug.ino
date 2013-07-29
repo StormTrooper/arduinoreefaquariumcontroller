64489094ea42eb410cfec0e597b3c63792953396
greg@mccarthy.za.net <greg@mccarthy.za.net@cb48e5b2-653c-70f1-f5b1-7746e4b3f057>
2013-07-29 17:40:44 +0000

    Upload to svn

---
+++ Debug.ino
@@ -0,0 +1,16 @@
+
+ // free RAM check for debugging. SRAM for ATmega328p = 2048Kb.
+int availableMemory() {
+    int size = 8*1024;
+    byte *buf;
+    while ((buf = (byte *) malloc(--size)) == NULL);
+        free(buf);
+    return size;
+}
+
+int freeRam () {  
+  extern int __heap_start, *__brkval;
+  int v;
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+}
+
