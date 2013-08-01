6fb384e7281d35b2e265810d2aece716a3f8af7f
greg@mccarthy.za.net <greg@mccarthy.za.net@cb48e5b2-653c-70f1-f5b1-7746e4b3f057>
2013-08-01 19:49:26 +0000

    Code cleanup

--- Ethernet.ino
+++ Ethernet.ino
@@ -1,3 +1,7 @@
+//-------------------------------------------------------------------------------------
+//					Ethernet Routines
+//-------------------------------------------------------------------------------------
+
 void TransmitData()
 {
     if (tempW != 0)                      //Values all still zero so Arduino has probably jut restarted. Do not send these values.
@@ -23,11 +27,11 @@ void TransmitData()
      //DEBUG_PRINTLN();
      // DEBUG_PRINT("<<< REQ ");
     
-      FillOutBuff (pH, 29, 5, 2);    //pH
-      FillOutBuff (Fanspeed, 42, 4, 0);    //Fanspeed
-      FillOutBuff (tempW, 54, 5, 2);    //Water
-      FillOutBuff (tempH, 67, 5, 2);    //Heatsink
-      FillOutBuff (tempS, 80, 5, 2);    //Sump
+      FillOutBuff (pH, 29, 5, 2);                                //pH
+      FillOutBuff (Fanspeed, 42, 4, 0);                          //Fanspeed
+      FillOutBuff (tempW, 54, 5, 2);                             //Water
+      FillOutBuff (tempH, 67, 5, 2);                             //Heatsink
+      FillOutBuff (tempS, 80, 5, 2);                             //Sump
       
       ether.browseUrl(PSTR("/update/"), cbuff , website, my_callback);
   
@@ -54,7 +58,7 @@ void FillOutBuff (float temp1, int dstart, int dlen, int zeros){
   dtostrf(temp1,dlen,zeros,tbuff);
   for (int i=0+dstart; i <= dstart+dlen-1; i++){
     cbuff[i] = tbuff[i-dstart];
-    if(cbuff[i] == ' ') cbuff[i] = '0';//if cbuff[i] = <SPACE> then cbuff[i] = a leading zero
+    if(cbuff[i] == ' ') cbuff[i] = '0';
   }
 }
 
